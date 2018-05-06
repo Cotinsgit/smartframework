768225ad3aa640ebcf1f3f9a09f6640703ccde1e
Cotinsgit <lsy044440@yahoo.com>
2018-05-06 20:38:10 +0800

    Initial commit

---
+++ src/main/java/org/smart4j/framework/util/ArrayUtil.java
@@ -0,0 +1,21 @@
+package org.smart4j.framework.util;
+
+/**
+ * 数组工具类
+ */
+
+public class ArrayUtil {
+    /**
+     * 判断数组是否非空
+     */
+    public static boolean isNotEmpty(Object[] array) {
+        return !ArrayUtil.isNotEmpty(array);
+    }
+
+    /**
+     * 判断数组是否为空
+     */
+    public static boolean isEmpty(Object[] array) {
+        return ArrayUtil.isEmpty(array);
+    }
+}
